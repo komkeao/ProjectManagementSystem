20e441ef5eaba41acd52b8bde5e7963c689d481c
Komkeao <kcsthai1995@gmail.com>
2017-07-05 18:22:06 +0700

    separate news page to new controller and design new page

---
+++ views/news/index.php
@@ -0,0 +1,80 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'ข่าวสาร';
+//$this->params['breadcrumbs'][] = ['label' => 'Employees', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<a href="#">
+    <div class="alert alert-bordered-dotted margin-bottom-3 padding-3"><!-- DEFAULT -->
+        <strong> การส่งเอกสารก่อนสอบ Midterm 2557 </strong>
+        <span style="color: black">- โดย ชิตสุธา สุ่มเล็ก (2015-03-02 12:09:33)</span>
+
+    </div>
+</a>
+<a href="#">
+    <div class="alert alert-bordered-dotted margin-bottom-3 padding-3"><!-- DEFAULT -->
+        <strong >  ให้นศ.แก้ไขชื่อที่ปรึกษาและเพิ่มหัวข้อโครงงานภายใน </strong>
+        <span style="color: black">- โดย ชิตสุธา สุ่มเล็ก (2015-03-02 12:09:33)</span>
+    </div>
+</a>
+<a href="#">
+    <div class="alert alert-bordered-dotted margin-bottom-3 padding-3"><!-- DEFAULT -->
+        <strong > กำหนดวันสอบ Midterm (ความก้าวหน้าครั้งที่ 2) </strong>
+        <span style="color: black">- โดย ชิตสุธา สุ่มเล็ก (2015-03-02 12:09:33)</span>
+    </div>
+</a>
+<a href="#">
+    <div class="alert alert-bordered-dotted margin-bottom-3 padding-3"><!-- DEFAULT -->
+        <strong> การส่งเอกสารก่อนสอบ Midterm 2557 </strong>
+        <span style="color: black">- โดย ชิตสุธา สุ่มเล็ก (2015-03-02 12:09:33)</span>
+
+    </div>
+</a>
+<a href="#">
+    <div class="alert alert-bordered-dotted margin-bottom-3 padding-3"><!-- DEFAULT -->
+        <strong >  ให้นศ.แก้ไขชื่อที่ปรึกษาและเพิ่มหัวข้อโครงงานภายใน </strong>
+        <span style="color: black">- โดย ชิตสุธา สุ่มเล็ก (2015-03-02 12:09:33)</span>
+    </div>
+</a>
+<a href="#">
+    <div class="alert alert-bordered-dotted margin-bottom-3 padding-3"><!-- DEFAULT -->
+        <strong > กำหนดวันสอบ Midterm (ความก้าวหน้าครั้งที่ 2) </strong>
+        <span style="color: black">- โดย ชิตสุธา สุ่มเล็ก (2015-03-02 12:09:33)</span>
+    </div>
+</a>
+<a href="#">
+    <div class="alert alert-bordered-dotted margin-bottom-3 padding-3"><!-- DEFAULT -->
+        <strong> การส่งเอกสารก่อนสอบ Midterm 2557 </strong>
+        <span style="color: black">- โดย ชิตสุธา สุ่มเล็ก (2015-03-02 12:09:33)</span>
+
+    </div>
+</a>
+<a href="#">
+    <div class="alert alert-bordered-dotted margin-bottom-3 padding-3"><!-- DEFAULT -->
+        <strong >  ให้นศ.แก้ไขชื่อที่ปรึกษาและเพิ่มหัวข้อโครงงานภายใน </strong>
+        <span style="color: black">- โดย ชิตสุธา สุ่มเล็ก (2015-03-02 12:09:33)</span>
+    </div>
+</a>
+<a href="#">
+    <div class="alert alert-bordered-dotted margin-bottom-3 padding-3"><!-- DEFAULT -->
+        <strong > กำหนดวันสอบ Midterm (ความก้าวหน้าครั้งที่ 2) </strong>
+        <span style="color: black">- โดย ชิตสุธา สุ่มเล็ก (2015-03-02 12:09:33)</span>
+    </div>
+</a>
+<!-- pagination -->
+<div class="text-center">
+    <ul class="pagination">
+        <li><a href="#">&laquo;</a></li>
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+        <li><a href="#">&raquo;</a></li>
+    </ul>
+</div>
+
+
