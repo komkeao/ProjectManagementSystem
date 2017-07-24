90ed86b58a5ab5adb003d76a6dae1de19af74095
komkeao2 <kcsthai1995@gmail.com>
2017-07-24 18:25:58 +0700

    add thai split word libbrary

---
+++ components/FileToTextHelper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MainUser
+ * Date: 24/7/2560
+ * Time: 17:54
+ */
+
+namespace app\components;
+
+
+class FileToTextHelper
+{
+    public static function helperGreetings() {
+        return "hello";
+    }
+
+}
