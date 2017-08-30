bb8439fd0a9f9c876cc08719426c7bd5416e8332
Ku Ku <yuparemee@gmail.com>
2017-08-30 17:53:44 +0700

    แก้management

--- views/adviser/management.php
+++ views/adviser/management.php
@@ -10,26 +10,30 @@ $this->params['breadcrumbs'][] = $this->title;
 <h1><?php echo $this->title ?></h1>
 
 
-<div class="col-md-9 col-sm-9">
+<div>
     <label>เรื่อง</label>
     <input type="text" name="contact[last_name]" value=""
            class="form-control required">
 </div>
-<div class="col-md-9 col-sm-9">
+<div>
     <label>ถึงอาจารย์</label>
     <input type="text" name="contact[last_name]" value=""
            class="form-control required">
-</div><div class="col-md-9 col-sm-9">
+</div>
+
+<div>
     <label>โดย</label>
     <input type="text" name="contact[last_name]" value=""
            class="form-control required">
 </div>
-<div class="col-md-9 col-sm-9">
+
+<div>
     <label>ข้อความ</label>
     <input type="text"style="height:150px"; name="contact[last_name]" value=""
            class="form-control required">
 </div>
-<div class="col-md-9 col-sm-9">
+
+<div>
 <button type="button" class="btn btn-default pull-right">ส่ง</button>
 </div>
 
